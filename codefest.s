865cdc9d73ad84ec40fa3866c9d4ee2d1ad06c17
Danny Schick <daniel.schick@colchestersd.org>
2016-12-03 17:24:36 +0000

    60 min commit. Can now read user input as string for derivative

--- codefest.s
+++ codefest.s
@@ -1,6 +1,9 @@
 .data
 welcomePrompt: .asciiz "Welcome to our calculator!"
-calcTypePrompt: .asciiz "What do you want to do?\n(1) Calculate a Derivative\n(2) Calculate an Integral"
+calcTypePrompt: .asciiz "What do you want to do?\n(1) Calculate a Derivative\n(2) Calculate an Integral\n"
+derivativePrompt: .asciiz "Enter in an equation. Maximum of 3 terms.\nExample: 4x^2+2x+4x^3\n"
+
+array: .space 60
 
 .text
 .globl main
@@ -19,11 +22,32 @@ loopstart:
     la $a0, calcTypePrompt
     syscall
     
-    # take user input and move that to $t0
+    # take user
     li $v0, 5
     syscall
-    move $t0, $a0
+    
+    li $t0, 1
+    beq $a0, $t0, derivative
     
     
 derivative:
     # this function will handle derivative input and calculation
+    # print prompt
+    li $v0, 4
+    la $a0, derivativePrompt
+    syscall
+    
+    li $v0, 8
+    la $a0, array
+    li $a1, 60
+    syscall
+    
+    li $v0, 4
+    syscall
+    
+    srl $a0, $a0, 5
+    li $v0, 4
+    syscall
+    
+    
+    
